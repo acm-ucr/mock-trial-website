1b2d3b0f825b264c24e98ec5673598252b0ac032
Victoria Zambrano <victoriaz20033@gmail.com>
2025-10-13 11:25:29 -0700

    Create resuable header & add to homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,7 @@
 import Header from "@/components/Header";
 
 const Home = () => {
-  return (        
-  <Header title ="About Us"/>
-  // <div className="">Home</div>;
-  );
-};        
+  return <Header title="About Us" />;
+};
 
 export default Home;
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,19 +4,14 @@ type HeaderProps = {
   title: string;
 };
 
-const Header = ({title} : HeaderProps) => {
-    return(
-        // <div className="flex w-full bg-mocktrial-blue border-t-8 border-yellow-500 justify-center items-center">
-        //     <p className="text-center text-black font-bold text-2xl mb-0 p-1">{title}</p>
-        // </div>
-        <div className="flex w-full h-full pt-2 pb-2 mb-2 bg-mocktrial-yellow">
-            <div className="flex w-full p-4 justify-center items-center bg-mocktrial-blue">
-                <p className="text-center text-5xl p-1 font-bold text-white">
-                    {title}
-                </p>
-            </div>
-        </div>
-    );
+const Header = ({ title }: HeaderProps) => {
+  return (
+    <div className="bg-mocktrial-yellow flex h-full w-full pt-2 pb-2">
+      <div className="bg-mocktrial-blue flex w-full items-center justify-center p-4">
+        <p className="p-1 text-center text-5xl font-bold text-white">{title}</p>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
