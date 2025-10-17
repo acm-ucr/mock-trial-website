882713a246356592b04994d1c2d899904f3de341
Yubin Zhen <yubinzhen020105@gmail.com>
2025-10-17 00:27:49 -0700

    added leftAchievements

--- src/app/achievements/page.tsx
+++ src/app/achievements/page.tsx
@@ -1,5 +1,13 @@
+import LeftAchievements from "@/components/achievements/leftAchievements";
+
 const Achievements = () => {
-  return <div className="">Achievements</div>;
+  return (
+    <div className="">
+      <LeftAchievements />
+
+      <div>Achievements</div>
+    </div>
+  );
 };
 
 export default Achievements;
---
+++ src/components/achievements/leftAchievements.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import Image, { StaticImageData } from "next/image";
+
+interface LeftAchievementsProps {
+  image: StaticImageData;
+  text: string;
+  size: string;
+}
+
+const LeftAchievements = ({ image, text, size }: LeftAchievementsProps) => {
+  return (
+    <div className="bg-mocktrial-blue flex h-full w-full flex-row text-white">
+      <div className="relative w-2/3">
+        <Image src={image} alt={text} className="object-cover" />
+      </div>
+
+      <div className="flex w-2/3 items-center justify-center text-center">
+        <div className={`${size} font-semibold`}>{text}</div>
+      </div>
+    </div>
+  );
+};
+
+export default LeftAchievements;
