df916c7c2045817073a6a8097de1296580a255c2
Yilin Liu <yilinliu741@gmail.com>
2025-10-16 22:48:32 -0700

    Deleted unnecessary styles

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -2,23 +2,25 @@ import Link from "next/link";
 
 const NotFound = () => {
   return (
-    <div className="relative flex h-[75vh] flex-col items-center bg-mocktrial-lightblue gap-2 font-bold md:gap-3">
-      <div className="relative -bottom-1/18 md:top-1/18 bg-mocktrial-darkblue h-1/8 w-full"></div>
-      <div className="flex flex-col h-2/3 justify-center items-center text-center gap-2 md:gap-6">
-        <p className="font-mocktrial-montserrat text-mocktrial-darkblue font-bold relative text-5xl md:text-9xl md:mt-6">404</p>
-        <p className="font-mocktrial-montserrat text-mocktrial-darkblue text-3xl font-bold md:text-7xl md:mb-6">
+    <div className="bg-mocktrial-lightblue relative flex h-[75vh] flex-col items-center gap-2 font-bold md:gap-3">
+      <div className="bg-mocktrial-darkblue relative -bottom-1/18 h-1/8 w-full md:top-1/18"></div>
+      <div className="flex h-2/3 flex-col items-center justify-center gap-2 text-center md:gap-6">
+        <p className="font-mocktrial-montserrat text-mocktrial-darkblue relative text-5xl font-bold md:mt-6 md:text-9xl">
+          404
+        </p>
+        <p className="font-mocktrial-montserrat text-mocktrial-darkblue text-3xl font-bold md:mb-6 md:text-7xl">
           Page not found
         </p>
-      
-          <Link
-            href="/"
-            className=" bg-mocktrial-darkblue text-white font-bold font-mocktrial-montserrat rounded-lg md:rounded-2xl md:p-2 text-md md:text-5xl px-10 md:px-20"
-          >
-            HOME
-          </Link>
+
+        <Link
+          href="/"
+          className="bg-mocktrial-darkblue font-mocktrial-montserrat text-md rounded-lg px-10 font-bold text-white md:rounded-2xl md:px-20 md:py-2 md:text-5xl"
+        >
+          HOME
+        </Link>
       </div>
- 
-      <div className="flex absolute bottom-0 bg-mocktrial-darkblue bg-bottom h-1/8 w-full"></div>
+
+      <div className="bg-mocktrial-darkblue absolute bottom-0 flex h-1/8 w-full"></div>
     </div>
   );
 };
