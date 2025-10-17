464d797af9e9ebf79ba0e37d269aad6dc342a748
atontalapur <advaith.mysuru@gmail.com>
2025-10-17 01:59:25 -0700

    Removed Text box and text

--- src/components/Home/JoinUs.tsx
+++ src/components/Home/JoinUs.tsx
@@ -7,17 +7,6 @@ export default function MockTrialLayout() {
       >
         <div className="mt-8 flex flex-1 items-center justify-center px-4 py-8 sm:mt-12 sm:px-6 md:-mt-70 md:px-8">
           <div className="w-full max-w-2xl">
-            <div className="bg-mocktrial-yellow rounded-2xl p-1.5 sm:rounded-3xl sm:p-2">
-              <div className="bg-mocktrial-blue rounded-xl px-4 py-8 text-center text-white sm:rounded-2xl sm:px-6 sm:py-10 md:px-8 md:py-12">
-                {/* <p className="text-base leading-relaxed font-semibold sm:text-lg md:text-xl lg:text-2xl">
-                  Mock Trial is a competitive team based activity where students
-                  simulate a real trial. Students act as both the attorneys and
-                  witnesses in the trial. Students are scored based on their
-                  arguments and performance.
-                </p> */}
-              </div>
-            </div>
-
             <div className="text-center sm:mt-15 md:mt-12 md:-mb-50">
               <p className="text-mocktrial-darkblue mb-4 text-2xl font-bold sm:mb-6 sm:text-3xl">
                 To join, email
