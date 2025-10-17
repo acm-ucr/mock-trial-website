03636cfbbadacc96b74dba45edda6e3cdf873fa8
ACM UCR <fake@domain.com>
2025-10-17 13:57:29 +0000

    convert images to webp

--- src/app/achievements/page.tsx
+++ src/app/achievements/page.tsx
@@ -1,5 +1,5 @@
 import RightAch from "@/components/RightAch";
-import Image from "@/public/test-image.jpg";
+import Image from "@/public/test-image.webp";
 
 const Achievements = () => {
   return (
