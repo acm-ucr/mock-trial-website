2d6598d62e4ffeaa1367ef1f41bb743b13185f9a
brysach <bryan.estradach@gmail.com>
2025-10-17 20:52:26 -0700

    Created About Us component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
-const Home = () => {
-  return <div className="">Home</div>;
-};
+import AboutUs from "@/components/Home/AboutUs";
 
+const Home = () => {
+  return(
+
+    <div>
+        <AboutUs />
+    </div>
+  );
+};
 export default Home;
---
+++ src/components/Home/AboutUs.tsx
@@ -0,0 +1,19 @@
+const AboutUs = () => {
+  return (
+    <div className="flex w-lg justify-center items-center rounded-3xl bg-yellow-400 m-4 p-3">
+
+      <div className="w-mlg bg-blue-800 p-6 rounded-2xl text-center text-xl text-white font-semibold">
+        <p>
+            Mock Trial is a competitive team based activity where students simulate a real trial.
+            Students act as both the attorneys and witnesses in the trial. Students are scored
+            based on their arguments and performance.
+        </p>
+      </div>
+    </div>
+    // <div>
+    //     <p>
+    //         Hello</p>
+    // </div>
+  );
+};
+export default AboutUs
