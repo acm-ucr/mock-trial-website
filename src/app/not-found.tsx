4c911236555541f0a6b6d7f66b450f1e54b2a262
Yilin Liu <yilinliu741@gmail.com>
2025-10-18 19:41:20 -0700

    Fixed

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -2,11 +2,11 @@ import Link from "next/link";
 
 const NotFound = () => {
   return (
-    <div className="bg-mocktrial-lightblue text-mocktrial-darkblue relative flex h-screen w-screen flex-col items-center gap-2 font-bold md:gap-3">
-      <div className="bg-mocktrial-darkblue relative h-1/8 w-full md:top-1/18"></div>
+    <div className="bg-mocktrial-lightblue text-mocktrial-darkblue relative flex h-screen w-screen flex-col items-center gap-2 md:gap-3">
+      <div className="bg-mocktrial-darkblue relative h-1/8 w-full md:top-1/18" />
       <div className="flex h-2/3 flex-col justify-center gap-2 text-center md:gap-6">
-        <p className="relative text-5xl md:mt-6 md:text-9xl">404</p>
-        <p className="text-3xl md:mb-6 md:text-7xl">Page not found</p>
+        <p className="relative text-5xl font-bold md:mt-6 md:text-9xl">404</p>
+        <p className="text-3xl font-bold md:mb-6 md:text-7xl">Page not found</p>
 
         <Link
           href="/"
@@ -16,7 +16,7 @@ const NotFound = () => {
         </Link>
       </div>
 
-      <div className="bg-mocktrial-darkblue absolute bottom-0 flex h-1/8 w-full"></div>
+      <div className="bg-mocktrial-darkblue absolute bottom-0 flex h-1/8 w-full" />
     </div>
   );
 };
