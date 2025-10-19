2ceef8f5d2dabebcee054082d45f6590c511d7eb
xxiyun <wesleylwu@gmail.com>
2025-10-18 17:30:04 -0700

    small changes

--- src/components/Home/JoinUs.tsx
+++ src/components/Home/JoinUs.tsx
@@ -1,11 +1,11 @@
 const JoinUs = () => {
   return (
-    <div className="bg-mocktrial-lightblue text-center">
-      <p className="text-mocktrial-darkblue mb-4 text-2xl font-bold sm:mb-6 sm:text-3xl">
+    <div className="text-mocktrial-darkblue text-center">
+      <p className="mb-4 text-2xl font-bold sm:mb-6 sm:text-3xl">
         To join, email
       </p>
-      <div className="bg-mocktrial-yellow inline-block rounded-full px-4 py-3 sm:px-6 sm:py-3.5 md:px-8 md:py-4">
-        <div className="text-mocktrial-darkblue text-sm font-semibold underline sm:text-base md:text-lg lg:text-xl">
+      <div className="bg-mocktrial-yellow inline-block rounded-full sm:px-6 sm:py-3.5 md:px-8 md:py-4">
+        <div className="text-sm font-semibold underline sm:text-base md:text-lg lg:text-xl">
           mocktrialucr@gmail.com
         </div>
       </div>
