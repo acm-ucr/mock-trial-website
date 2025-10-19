1c0c7c6872eed94975f12b080892fb371d115552
brysach <bryan.estradach@gmail.com>
2025-10-18 19:23:07 -0700

    Made requested PR changes

--- src/components/Home/AboutUs.tsx
+++ src/components/Home/AboutUs.tsx
@@ -1,7 +1,7 @@
 const AboutUs = () => {
   return (
-    <div className="m-4 w-lg mx-auto rounded-3xl bg-mocktrial-yellow p-3">
-      <div className="rounded-2xl bg-mocktrial-blue p-6 text-center text-xl font-semibold text-white font-mocktrial-montserrat">
+    <div className="bg-mocktrial-yellow m-4 mx-auto w-lg rounded-3xl p-3">
+      <div className="bg-mocktrial-blue font-mocktrial-montserrat rounded-2xl p-6 text-center text-xl font-semibold text-white">
         <p>
           Mock Trial is a competitive team based activity where students
           simulate a real trial. Students act as both the attorneys and
