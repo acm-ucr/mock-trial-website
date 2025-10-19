41c31caa90129e1da0a8175be10bc1be83f361a5
Colin <colintruong168@gmail.com>
2025-10-18 19:55:37 -0700

    Worked on the 'What is Mock Trial?' component

---
+++ src/components/faqs/WhatIsMock.tsx
@@ -0,0 +1,12 @@
+import { IoIosArrowForward } from "react-icons/io";
+const WhatIsMock = () => {
+  return (
+    <div className="bg-mocktrial-seablue text-mocktrial-blue border-mocktrial-blue flex w-1/2 rounded-xl border-8 p-5">
+      <div className="text-mocktrial-blue pl-4 text-3xl font-bold">
+        What is Mock Trial?
+      </div>
+      <IoIosArrowForward className="ml-auto h-10 w-10"></IoIosArrowForward>
+    </div>
+  );
+};
+export default WhatIsMock;
