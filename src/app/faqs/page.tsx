562d4ef329d5876c68e844c4992cdcba8560d969
Colin <colintruong168@gmail.com>
2025-10-20 00:09:36 -0700

    Finished request changes

--- src/app/faqs/page.tsx
+++ src/app/faqs/page.tsx
@@ -1,5 +1,13 @@
+import WhatIsMock from "@/components/faqs/WhatIsMock";
 const FAQS = () => {
-  return <div className="">FAQS</div>;
+  return (
+    <div>
+      <p>FAQS</p>
+      <div className="flex h-screen w-screen items-center justify-center">
+        <WhatIsMock />
+      </div>
+    </div>
+  );
 };
 
 export default FAQS;
--- src/components/faqs/WhatIsMock.tsx
+++ src/components/faqs/WhatIsMock.tsx
@@ -3,9 +3,9 @@ const WhatIsMock = () => {
   return (
     <div className="bg-mocktrial-seablue text-mocktrial-blue border-mocktrial-blue flex w-1/2 rounded-xl border-8 p-5">
       <div className="text-mocktrial-blue pl-4 text-3xl font-bold">
-        What is Mock Trial?
+        <p>What is Mock Trial?</p>
       </div>
-      <IoIosArrowForward className="ml-auto h-10 w-10"></IoIosArrowForward>
+      <IoIosArrowForward className="ml-auto h-10 w-10" />
     </div>
   );
 };
