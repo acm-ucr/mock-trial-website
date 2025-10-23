89b0edf92e104e700daa2bce6ed6e1eb4105c756
atontalapur <advaith.mysuru@gmail.com>
2025-10-23 15:17:48 -0700

    Changed color, font size, and sizing of members.

--- src/components/Board/BoardSecondRow.tsx
+++ src/components/Board/BoardSecondRow.tsx
@@ -4,37 +4,39 @@ import pascaleProfilePicture from "@/public/board/pascale_gibbs.webp";
 
 const BoardSecondRow = () => {
   return (
-    <div className="flex flex-col justify-center gap-20 md:flex-row">
-      <div className="pt-20 text-center">
-        <div className="bg-mocktrial-yellow relative inset-0 mx-auto h-[300px] w-[300px] rounded-3xl">
-          <div className="absolute z-10 -mt-28">
+    <div className="flex flex-col justify-center gap-20 py-40 md:flex-row">
+      <div className="text-center">
+        <div className="relative mx-auto h-100 w-100">
+          <div className="absolute inset-0 rounded-3xl bg-linear-to-t from-[#E7C67E] to-[#E1AF42]"></div>
+          <div className="absolute -top-40 right-0 left-0 z-10">
             <Image
               src={tejalProfilePicture}
               alt="Tejal Prabhu"
-              className="h-full w-full rounded-3xl object-cover"
+              className="h-140 w-full rounded-3xl object-cover"
             />
           </div>
         </div>
-        <p className="text-mocktrial-darkblue mt-6 text-2xl font-bold">
+        <p className="text-mocktrial-darkblue mt-6 text-3xl font-bold">
           Tejal Prabhu
         </p>
-        <p className="text-mocktrial-darkblue text-lg">Co-Coordinator</p>
+        <p className="text-mocktrial-darkblue text-2xl">Co-Coordinator</p>
       </div>
 
-      <div className="pt-20 text-center">
-        <div className="bg-mocktrial-yellow relative inset-0 mx-auto h-[300px] w-[300px] rounded-3xl">
-          <div className="absolute z-10 -mt-31 mr-2">
+      <div className="text-center">
+        <div className="relative mx-auto h-100 w-100">
+          <div className="absolute inset-0 rounded-3xl bg-linear-to-t from-[#E7C67E] to-[#E1AF42]"></div>
+          <div className="absolute -top-40 right-0 left-2 z-10">
             <Image
               src={pascaleProfilePicture}
               alt="Pascale Gibbs"
-              className="h-full w-full rounded-3xl object-cover"
+              className="h-140 w-95 rounded-3xl object-cover"
             />
           </div>
         </div>
-        <p className="text-mocktrial-darkblue mt-6 text-2xl font-bold">
+        <p className="text-mocktrial-darkblue mt-6 text-3xl font-bold">
           Pascale Gibbs
         </p>
-        <p className="text-mocktrial-darkblue text-lg">Co-Coordinator</p>
+        <p className="text-mocktrial-darkblue text-2xl">Co-Coordinator</p>
       </div>
     </div>
   );
