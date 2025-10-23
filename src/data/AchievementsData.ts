2b2f5be1ff5d360d4ed628fd113ab2c116032fc1
XiyuanWu <xiyuan.24706826@gmail.com>
2025-10-22 19:11:22 -0700

    ach grid

---
+++ src/data/AchievementsData.ts
@@ -0,0 +1,41 @@
+import Image1 from "@/public/achievements/1.webp";
+import Image2 from "@/public/achievements/2.webp";
+import Image3 from "@/public/achievements/3.webp";
+import Image4 from "@/public/achievements/4.webp";
+import Image5 from "@/public/achievements/5.webp";
+import Image6 from "@/public/achievements/6.webp";
+
+const achievementsData = [
+  {
+    image: Image1,
+    text: "2024-2025 Spamta Winner",
+    size: "text-5xl",
+  },
+  {
+    image: Image2,
+    text: "19 Individual Awards",
+    size: "text-5xl",
+  },
+  {
+    image: Image3,
+    text: "6 Top 5 Team Finishes",
+    size: "text-5xl",
+  },
+  {
+    image: Image4,
+    text: "Regional Spamta Winner",
+    size: "text-5xl",
+  },
+  {
+    image: Image5,
+    text: "First Bid to the opening round championship series in 7 years",
+    size: "text-4xl",
+  },
+  {
+    image: Image6,
+    text: "Rank within top 25% of team in the country",
+    size: "text-4xl",
+  },
+];
+
+export default achievementsData;
