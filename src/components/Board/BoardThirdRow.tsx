d788395eb7630730c8d3702419daed99258b82a0
Yilin Liu <yilinliu741@gmail.com>
2025-10-22 23:33:18 -0700

    Aligned the boxes and pictures

--- src/components/Board/BoardThirdRow.tsx
+++ src/components/Board/BoardThirdRow.tsx
@@ -4,12 +4,17 @@ import SriVidyaCharepalli from "@/public/board/sri_vidya_charepalli.webp"
 
 const BoardThirdRow = () => {
   return (
-    <div className="grid grid-cols-2 ">
-        <div className="flex relative w-[35vh] h-[35vh] rounded-4xl bg-gradient-to-b from-mocktrial-yellow to-amber-100" />
-        <div className="flex relative w-[35vh] h-[35vh] rounded-4xl bg-gradient-to-b from-mocktrial-yellow to-amber-100" />
-        <Image src = {AbinayaHariharan} alt = "Abinaya Hariharan" className="w-[50vh]" />
-        <Image src = {SriVidyaCharepalli} alt = "Sri Vidya Charepalli" />
-        <div className="text-mocktrial-darkblue relative text-4xl items-center font-bold">
+    <div className="flex items-center justify-center gap-16 p-20">
+        <div className="flex relative w-80 h-80 rounded-4xl bg-gradient-to-b from-mocktrial-yellow to-amber-100 mx-14" />
+        <div className="absolute w-104 mb-58 mr-95" >
+            <Image src = {AbinayaHariharan} alt = "Abinaya Hariharan" className="w-full h-full object-cover" />
+        </div>
+        <div className="flex relative w-80 h-80 rounded-4xl bg-gradient-to-b from-mocktrial-yellow to-amber-100" />
+        <div className="absolute w-75 mb-16 ml-120" >
+          <Image src = {SriVidyaCharepalli} alt = "Sri Vidya Charepalli" className="w-full h-full object-cover" />
+        </div>
+        {/*
+        <div className="flex flex-row text-mocktrial-darkblue relative text-4xl items-center font-bold inset-x-0">
             <p>Abinaya Hariharan</p>
             <p>Sri Vidya Charepalli</p>
         </div>
@@ -17,6 +22,7 @@ const BoardThirdRow = () => {
             <p>Co-Fundraising Chair</p>
             <p>Co-Fundraising Chair</p>
         </div>
+        */}
 
     </div>
   )
