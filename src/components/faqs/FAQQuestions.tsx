0ec9312dd9e3c93b86bbb7266f6411b28f758aa3
Victoria Zambrano <victoriaz20033@gmail.com>
2025-10-27 13:55:23 -0700

    Finalizing FAQ mapping changes

--- src/components/faqs/FAQQuestions.tsx
+++ src/components/faqs/FAQQuestions.tsx
@@ -2,19 +2,31 @@ import { IoIosArrowForward } from "react-icons/io";
 
 interface FAQQuestion {
   question: string;
+  id?: string;
 }
 
 interface FAQProps {
   faqData: FAQQuestion[];
 }
 
+const FAQItem: React.FC<{ item: FAQQuestion }> = ({ item }) => (
+  <div className="flex w-full items-center py-2">
+    <p className="text-mocktrial-blue pl-4 text-3xl font-bold">
+      {item.question}
+    </p>
+    <IoIosArrowForward className="ml-auto h-10 w-10" />
+  </div>
+);
+
 const FAQQuestions = ({ faqData }: FAQProps) => {
   return (
-    <div className="bg-mocktrial-seablue text-mocktrial-blue border-mocktrial-blue mx-auto flex w-1/2 rounded-xl border-8 p-5">
-      {faqData.map((item, index) => (
-        <div key={item.id ?? index} className="flex w-full items-center py-2">
-          <span className="text-lg font-medium">{item.question}</span>
-          <IoIosArrowForward className="ml-auto h-10 w-10" />
+    <div className="flex flex-col gap-6 py-8">
+      {faqData.map((q) => (
+        <div
+          className="bg-mocktrial-seablue text-mocktrial-blue border-mocktrial-blue mx-auto my-0.5 flex w-1/2 rounded-xl border-8 p-5 last:mb-12"
+          key={(q as FAQQuestion).id ?? q.question}
+        >
+          <FAQItem item={q as FAQQuestion} />
         </div>
       ))}
     </div>
