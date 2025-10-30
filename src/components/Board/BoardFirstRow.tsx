d32aff0c0d170c7a9d1d8d253372370ea6c72099
colintruong <colintruong168@gmail.com>
2025-10-30 14:41:03 -0700

    Hopefully changes fix the cropping issue

--- src/components/Board/BoardFirstRow.tsx
+++ src/components/Board/BoardFirstRow.tsx
@@ -4,39 +4,35 @@ import VicePresident from "@/public/board/keaton_bunker.webp";
 
 const BoardFirstRow = () => {
   return (
-    <div className="mt-50 flex h-screen items-start justify-center gap-20">
-      <div className="flex w-1/4 flex-col items-center">
-        <div className="bg-mocktrial-yellow relative aspect-square w-full overflow-visible rounded-3xl">
-          <Image
-            src={President}
-            alt="Natalia Teglia"
-            fill
-            className="-translate-y-17 scale-140 rounded-3xl object-cover object-bottom"
-          />
+    <div className="text-mocktrial-darkblue flex flex-col justify-center gap-20 py-40 text-center md:flex-row">
+      <div>
+        <div className="relative h-100 w-100">
+          <div className="absolute inset-0 rounded-3xl bg-linear-to-t from-[#E7C67E] to-[#E1AF42]"></div>
+          <div className="absolute -top-53">
+            <Image
+              src={President}
+              alt="Natalia Teglia"
+              className="h-full w-full scale-130 object-cover"
+            />
+          </div>
         </div>
-        <p className="text-mocktrial-darkblue mt-5 text-center text-[2.5rem] font-extrabold">
-          Natalia Teglia
-        </p>
-        <p className="text-mocktrial-darkblue -mt-3 text-center text-2xl font-semibold">
-          President
-        </p>
+        <p className="mt-6 text-3xl font-bold">Natalia Teglia</p>
+        <p className="text-2xl font-bold">President</p>
       </div>
 
-      <div className="flex w-1/4 flex-col items-center">
-        <div className="bg-mocktrial-yellow relative aspect-square w-full overflow-visible rounded-3xl">
-          <Image
-            src={VicePresident}
-            alt="Keaton Bunker"
-            fill
-            className="origin-bottom -translate-x-5 -translate-y-23 scale-90 overflow-visible rounded-3xl object-cover"
-          />
+      <div>
+        <div className="relative h-100 w-100">
+          <div className="absolute inset-0 rounded-3xl bg-linear-to-t from-[#E7C67E] to-[#E1AF42]"></div>
+          <div className="absolute -top-58.5 -left-10">
+            <Image
+              src={VicePresident}
+              alt="Keaton Bunker"
+              className="h-full w-full scale-80 object-cover"
+            />
+          </div>
         </div>
-        <p className="text-mocktrial-darkblue mt-5 text-center text-[2.5rem] font-extrabold">
-          Keaton Bunker
-        </p>
-        <p className="text-mocktrial-darkblue -mt-3 text-center text-2xl font-semibold">
-          Vice President
-        </p>
+        <p className="mt-6 text-3xl font-bold">Keaton Bunker</p>
+        <p className="text-2xl">Vice President</p>
       </div>
     </div>
   );
