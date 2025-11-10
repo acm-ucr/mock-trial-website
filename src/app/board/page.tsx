6e219483b5d9650cf98237f7c19bae5bacf1fc25
xxiyun <wesleylwu@gmail.com>
2025-11-10 14:34:40 -0800

    made demo

--- src/app/board/page.tsx
+++ src/app/board/page.tsx
@@ -1,11 +1,13 @@
 import Header from "@/components/Header";
 import Board1 from "@/components/Board/Board";
+import Demo from "@/components/Home/Demo";
 
 const Board = () => {
   return (
     <div className="bg-mocktrial-lightblue">
       <Header title="Board" />
       <Board1 />
+      <Demo />
     </div>
   );
 };
