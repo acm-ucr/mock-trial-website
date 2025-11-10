fae49febacdc81caeae1cdfa31986e81d1543ac7
xxiyun <wesleylwu@gmail.com>
2025-11-10 14:36:40 -0800

    s

--- src/components/Home/Demo.tsx
+++ src/components/Home/Demo.tsx
@@ -1,31 +1,27 @@
 "use client";
-import { motion } from "motion/react";
+// import { motion } from "motion/react";
 
 const Demo = () => {
-  const values = [{ name: "Item 1" }, { name: "Item 2" }, { name: "Item 3" }];
-  const sampleAnimation = {
-    initial: { scale: 0 },
-    whileInView: { scale: 1, transition: { duration: 4.0 } },
-  };
+  //   const values = [{ name: "Item 1" }, { name: "Item 2" }, { name: "Item 3" }];
+  //   const sampleAnimation = {
+  //     initial: { scale: 0 },
+  //     whileInView: { scale: 1, transition: { duration: 4.0 } },
+  //   };
 
   return (
     <div className="flex flex-col items-center justify-center gap-8 p-20 text-3xl">
       {/* <motion.div animate={{ rotate: 360 }}>
         Some Text
       </motion.div> */}
-
       {/* <motion.div initial={{ scale: 0 }} animate={{ scale: 1 }}>
         Some Text
       </motion.div> */}
-
       {/* <motion.div initial={{ scale: 0 }} whileInView={{ scale: 1 }}>
         Some Text
       </motion.div> */}
-
       {/* <motion.div initial={{ scale: 0 }} whileInView={{ scale: 1 }} viewport={{ once: true }}>
         Some Text
       </motion.div> */}
-
       {/* {values.map(({ name }, index) => (
         <motion.div
           key={index}
@@ -36,14 +32,13 @@ const Demo = () => {
           {name}
         </motion.div>
       ))} */}
-
       {/* <motion.div whileHover={{ scale: 1.05 }}>
         Some Text
       </motion.div> */}
-
       {/* <motion.div variants={sampleAnimation} initial="initial" whileInView="whileInView">
         Some Text
       </motion.div> */}
+      hello
     </div>
   );
 };
