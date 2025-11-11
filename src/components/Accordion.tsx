4b10bf10a906fd8bbacf81f14af87979d4196a7d
xxiyun <wesleylwu@gmail.com>
2025-11-10 23:42:56 -0800

    accrdion clean up

--- src/components/Accordion.tsx
+++ src/components/Accordion.tsx
@@ -24,13 +24,13 @@ const AccordionTrigger = React.forwardRef<
     <AccordionPrimitive.Trigger
       ref={ref}
       className={cn(
-        "text-acm-gray-500 flex flex-1 items-center justify-between py-4 text-left text-2xl font-bold transition-all hover:underline [&[data-state=open]>svg]:rotate-180",
+        "flex items-center justify-between py-4 text-2xl font-bold hover:underline [&[data-state=open]>svg]:rotate-180",
         className,
       )}
       {...props}
     >
       {children}
-      <IoIosArrowForward className="text-muted-foreground h-10 w-10 shrink-0 transition-transform duration-200" />
+      <IoIosArrowForward className="h-10 w-10 shrink-0 duration-200" />
     </AccordionPrimitive.Trigger>
   </AccordionPrimitive.Header>
 ));
@@ -42,7 +42,7 @@ const AccordionContent = React.forwardRef<
 >(({ className, children, ...props }, ref) => (
   <AccordionPrimitive.Content
     ref={ref}
-    className="data-[state=closed]:animate-accordion-up data-[state=open]:animate-accordion-down text-acm-gray-500 overflow-hidden text-2xl font-medium"
+    className="data-[state=closed]:animate-accordion-up data-[state=open]:animate-accordion-down overflow-hidden text-2xl font-medium"
     {...props}
   >
     <div className={cn("pt-0 pb-4", className)}>{children}</div>
@@ -50,39 +50,4 @@ const AccordionContent = React.forwardRef<
 ));
 AccordionContent.displayName = AccordionPrimitive.Content.displayName;
 
-const CohortTrigger = React.forwardRef<
-  React.ElementRef<typeof AccordionPrimitive.Trigger>,
-  React.ComponentPropsWithoutRef<typeof AccordionPrimitive.Trigger>
->(({ className, children, ...props }, ref) => (
-  <AccordionPrimitive.Header className="flex justify-center">
-    <AccordionPrimitive.Trigger
-      ref={ref}
-      className={cn(
-        "text-acm-gray-500 flex items-center justify-center py-4 text-4xl font-bold transition-all hover:underline [&[data-state=open]>svg]:rotate-180",
-        className,
-      )}
-      {...props}
-    >
-      {children}
-      <IoIosArrowForward className="!text-acm-blue-600 text-muted-foreground ml-4 shrink-0 transition-transform duration-200" />
-    </AccordionPrimitive.Trigger>
-  </AccordionPrimitive.Header>
-));
-CohortTrigger.displayName = AccordionPrimitive.Trigger.displayName;
-
-const CohortItem = React.forwardRef<
-  React.ElementRef<typeof AccordionPrimitive.Item>,
-  React.ComponentPropsWithoutRef<typeof AccordionPrimitive.Item>
->(({ className, ...props }, ref) => (
-  <AccordionPrimitive.Item ref={ref} className={cn("", className)} {...props} />
-));
-CohortItem.displayName = "AccordionItem";
-
-export {
-  Accordion,
-  AccordionItem,
-  AccordionTrigger,
-  AccordionContent,
-  CohortTrigger,
-  CohortItem,
-};
+export { Accordion, AccordionItem, AccordionTrigger, AccordionContent };
