220d89aef53a6f55d3522d15348129ea0f56ee59
Jeffrey <Jeffx2016@gmail.com>
2025-11-18 10:47:12 -0800

    merge conflicts

--- src/components/faqs/FAQQuestionsCard.tsx
+++ src/components/faqs/FAQQuestionsCard.tsx
@@ -28,15 +28,16 @@ const FAQQuestionsCard = ({ item }: { item: FAQQuestion }) => {
       initial="initial"
       whileInView="whileInView"
       viewport={{ once: true }}
-      className="bg-mocktrial-seablue text-mocktrial-blue border-mocktrial-blue mx-auto my-0.5 flex w-4/5 flex-col rounded-xl border-6 p-5 last:mb-26 md:w-2/3 md:border-7 md:last:mb-6 lg:border-8"
+      transition={{ duration: 1 }}
+      className="bg-mocktrial-seablue text-mocktrial-blue border-mocktrial-blue mx-auto my-0.5 flex w-4/5 flex-col rounded-lg border-4 p-3 last:mb-20 md:w-2/3 md:border-5 md:last:mb-4 lg:border-6"
     >
       <AccordionItem value={`faq-${item.question}`}>
-        <AccordionTrigger className="flex w-full items-center py-2">
-          <p className="text-mocktrial-blue pl-4 text-xl font-bold md:text-2xl lg:text-3xl">
+        <AccordionTrigger className="flex w-full items-center py-1.5">
+          <p className="text-mocktrial-blue pl-3 text-lg font-semibold md:text-xl lg:text-2xl">
             {item.question}
           </p>
         </AccordionTrigger>
-        <AccordionContent className="text-mocktrial-blue px-4 py-2 text-lg md:text-xl">
+        <AccordionContent className="text-mocktrial-blue px-3 py-1.5 text-base md:text-lg">
           {item.answer}
         </AccordionContent>
       </AccordionItem>
