5a1b99cd77a75aa629d62b1142480c0a8d69af7e
Jeffrey <Jeffx2016@gmail.com>
2025-11-24 11:50:07 -0800

    fixed bug where  would increase scale when mouse is below button

--- src/components/Home/AboutUs.tsx
+++ src/components/Home/AboutUs.tsx
@@ -37,16 +37,14 @@ const AboutUs = () => {
           To join, email
         </p>
 
-        <motion.div whileHover={{ scale: 1.05 }} className="inline-block">
           <Link href="mailto:mocktrialucr@gmail.com" aria-label="Email">
-            <div className="bg-mocktrial-yellow mb-25 inline-block rounded-full px-6 py-3.5 sm:px-8 sm:py-4 md:px-8 md:py-4 lg:px-10 lg:py-4.5">
+            <motion.div whileHover={{ scale: 1.05 }} className="inline-blaock bg-mocktrial-yellow mb-25 inline-block rounded-full px-6 py-3.5 sm:px-8 sm:py-4 md:px-8 md:py-4 lg:px-10 lg:py-4.5">
               <div className="text-sm font-semibold underline sm:text-base md:text-lg lg:text-xl">
                 mocktrialucr@gmail.com
               </div>
-            </div>
+            </motion.div>
           </Link>
         </motion.div>
-      </motion.div>
     </div>
   );
 };
